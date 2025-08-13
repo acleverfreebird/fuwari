84b1895d63081de7a28ce61b0e8116fa3664d814
2280136516 <2280136516@qq.com>
2025-08-13 12:05:31 +0000

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -28,80 +28,85 @@ import netlify from "@astrojs/netlify";
 
 import vercel from "@astrojs/vercel";
 
-import react from "@astrojs/react";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.freebird.asia/",
   base: "/",
   trailingSlash: "always",
 
-  integrations: [tailwind({
-      nesting: true,
-  }), swup({
-      theme: false,
-      animationClass: "transition-swup-", // see https://swup.js.org/options/#animationselector
-      // the default value `transition-` cause transition delay
-      // when the Tailwind class `transition-all` is used
-      containers: ["main", "#toc"],
-      smoothScrolling: true,
-      cache: true,
-      preload: true,
-      accessibility: true,
-      updateHead: true,
-      updateBodyClass: false,
-      globalInstance: true,
-  }), icon({
-      include: {
-          "preprocess: vitePreprocess(),": ["*"],
-          "fa6-brands": ["*"],
-          "fa6-regular": ["*"],
-          "fa6-solid": ["*"],
-      },
-  }), expressiveCode({
-      themes: [expressiveCodeConfig.theme, expressiveCodeConfig.theme],
-      plugins: [
-          pluginCollapsibleSections(),
-          pluginLineNumbers(),
-          pluginLanguageBadge(),
-          pluginCustomCopyButton()
-      ],
-      defaultProps: {
-          wrap: true,
-          overridesByLang: {
-              'shellsession': {
-                  showLineNumbers: false,
+  integrations: [
+      tailwind({
+          nesting: true,
+      }),
+      swup({
+          theme: false,
+          animationClass: "transition-swup-", // see https://swup.js.org/options/#animationselector
+          // the default value `transition-` cause transition delay
+          // when the Tailwind class `transition-all` is used
+          containers: ["main", "#toc"],
+          smoothScrolling: true,
+          cache: true,
+          preload: true,
+          accessibility: true,
+          updateHead: true,
+          updateBodyClass: false,
+          globalInstance: true,
+      }),
+      icon({
+          include: {
+              "preprocess: vitePreprocess(),": ["*"],
+              "fa6-brands": ["*"],
+              "fa6-regular": ["*"],
+              "fa6-solid": ["*"],
+          },
+      }),
+      expressiveCode({
+          themes: [expressiveCodeConfig.theme, expressiveCodeConfig.theme],
+          plugins: [
+              pluginCollapsibleSections(),
+              pluginLineNumbers(),
+              pluginLanguageBadge(),
+              pluginCustomCopyButton()
+          ],
+          defaultProps: {
+              wrap: true,
+              overridesByLang: {
+                  'shellsession': {
+                      showLineNumbers: false,
+                  },
               },
           },
-      },
-      styleOverrides: {
-          codeBackground: "var(--codeblock-bg)",
-          borderRadius: "0.75rem",
-          borderColor: "none",
-          codeFontSize: "0.875rem",
-          codeFontFamily: "'JetBrains Mono Variable', ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, 'Liberation Mono', 'Courier New', monospace",
-          codeLineHeight: "1.5rem",
-          frames: {
-              editorBackground: "var(--codeblock-bg)",
-              terminalBackground: "var(--codeblock-bg)",
-              terminalTitlebarBackground: "var(--codeblock-topbar-bg)",
-              editorTabBarBackground: "var(--codeblock-topbar-bg)",
-              editorActiveTabBackground: "none",
-              editorActiveTabIndicatorBottomColor: "var(--primary)",
-              editorActiveTabIndicatorTopColor: "none",
-              editorTabBarBorderBottomColor: "var(--codeblock-topbar-bg)",
-              terminalTitlebarBorderBottomColor: "none"
+          styleOverrides: {
+              codeBackground: "var(--codeblock-bg)",
+              borderRadius: "0.75rem",
+              borderColor: "none",
+              codeFontSize: "0.875rem",
+              codeFontFamily: "'JetBrains Mono Variable', ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, 'Liberation Mono', 'Courier New', monospace",
+              codeLineHeight: "1.5rem",
+              frames: {
+                  editorBackground: "var(--codeblock-bg)",
+                  terminalBackground: "var(--codeblock-bg)",
+                  terminalTitlebarBackground: "var(--codeblock-topbar-bg)",
+                  editorTabBarBackground: "var(--codeblock-topbar-bg)",
+                  editorActiveTabBackground: "none",
+                  editorActiveTabIndicatorBottomColor: "var(--primary)",
+                  editorActiveTabIndicatorTopColor: "none",
+                  editorTabBarBorderBottomColor: "var(--codeblock-topbar-bg)",
+                  terminalTitlebarBorderBottomColor: "none"
+              },
+              textMarkers: {
+                  delHue: 0,
+                  insHue: 180,
+                  markHue: 250
+              }
           },
-          textMarkers: {
-              delHue: 0,
-              insHue: 180,
-              markHue: 250
+          frames: {
+              showCopyToClipboardButton: false,
           }
-      },
-      frames: {
-          showCopyToClipboardButton: false,
-      }
-  }), svelte(), sitemap(), react()],
+      }),
+      svelte(),
+      sitemap(),
+    ],
 
   markdown: {
       remarkPlugins: [
