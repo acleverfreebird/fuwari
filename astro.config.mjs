1f684f51d5c3a2955a2c9777e5a5dd296ed2b12f
free bird <2280136516@qq.com>
2025-08-23 09:26:45 +0800

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,7 +27,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://www.freebird.asia/",
+	site: "https://freebird2913.tech/",
 	base: "/",
 	trailingSlash: "always",
 
