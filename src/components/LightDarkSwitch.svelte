ecce78503f56523d55e286db897b62587b2b7be4
free bird <2280136516@qq.com>
2025-12-21 13:20:15 +0800

    fixed up

--- src/components/LightDarkSwitch.svelte
+++ src/components/LightDarkSwitch.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+type Props = {};
+
 import { AUTO_MODE, DARK_MODE, LIGHT_MODE } from "@constants/constants.ts";
 import I18nKey from "@i18n/i18nKey";
 import { i18n } from "@i18n/translation";
